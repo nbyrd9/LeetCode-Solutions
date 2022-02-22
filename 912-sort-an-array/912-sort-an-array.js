81670f26664c3d30462c9d56c067fc41c45c4425
nbyrd9 <50628304+nbyrd9@users.noreply.github.com>
2022-02-22 16:13:52 -0500

    Time: 3401 ms (20.12%), Space: 51.9 MB (73.58%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.js
+++ 912-sort-an-array/912-sort-an-array.js
@@ -3,6 +3,23 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-   return nums.sort((a, b) => a - b);
-   
+  return insertionSort(nums);
+  
 };
+
+
+function insertionSort(inputArr) {
+    let n = inputArr.length;
+        for (let i = 1; i < n; i++) {
+            // Choosing the first element in our unsorted subarray
+            let current = inputArr[i];
+            // The last element of our sorted subarray
+            let j = i-1; 
+            while ((j > -1) && (current < inputArr[j])) {
+                inputArr[j+1] = inputArr[j];
+                j--;
+            }
+            inputArr[j+1] = current;
+        }
+    return inputArr;
+}
